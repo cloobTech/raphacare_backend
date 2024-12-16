4e84c60437209eeda6651fcfbb0895a32f106fe9
Olamide C. Bello <cloobtechse@gamil.com>
2024-12-16 16:05:21 +0100

    fix helper func to format datetime

--- services/consultations/helpers.py
+++ services/consultations/helpers.py
@@ -1,6 +1,6 @@
-from datetime import datetime
 from storage import DBStorage as DB
 from models.appointment import Appointment, AppointmentStatus
+from utils.parse_datetime import parse_and_format_datetime
 
 
 # format for datetime used within the app
@@ -11,8 +11,8 @@ async def is_slot_available(storage: DB, data: dict) -> bool:
     """Check if appointment slot is available"""
 
     med_pract_id = data["medical_practitioner_id"]
-    start_time = datetime.strptime(data["appointment_start_time"], TIME_FORMAT)
-    end_time = datetime.strptime(data["appointment_end_time"], TIME_FORMAT)
+    start_time = parse_and_format_datetime(data["appointment_start_time"])
+    end_time = parse_and_format_datetime(data["appointment_end_time"])
 
     existing_appointment = await storage.filter(Appointment, Appointment.medical_practitioner_id == med_pract_id, Appointment.appointment_start_time >= start_time, Appointment.appointment_end_time <= end_time, Appointment.appointment_status.in_([AppointmentStatus.pending, AppointmentStatus.confirmed]), fetch_one=True)
 
